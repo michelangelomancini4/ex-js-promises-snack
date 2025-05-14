d44ad13a07dae4c92045f65d5cb075a25080dd6b
michelangelomancini <michelangelomancini8@gmail.com>
2025-05-14 09:12:25 +0200

    Bonus snack-1 done

--- snack-1/script.js
+++ snack-1/script.js
@@ -8,6 +8,30 @@ function getPostTitle(id) {
     return promessa;
 }
 
-getPostTitle(1)
+getPostTitle(4)
     .then(obj => console.log(obj.title))
     .catch(error => console.error(error))
+
+
+
+// ***********  BONUS SNACK ************
+
+
+function getPost(id) {
+
+    return fetch(`https://dummyjson.com/posts/${id}`)
+        .then(response => response.json())
+        .then(post => {
+
+            return fetch(`https://dummyjson.com/users/${post.userId}`)
+                .then(response => response.json())
+                .then(user => {
+                    const postWithUser = { ...post, user: user };
+                    return postWithUser;
+                });
+        });
+}
+
+getPost(1)
+    .then(post => console.log(post))
+    .catch(error => console.error("Errore:", error));
